8a4e31bf0c748f53d0de3ee2508f78a330bd68e1
Your Name <your.email@example.com>
2026-02-11 17:19:02 +0700

    tubes rpl

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- JANGAN LUPA TAMBAHKAN INI
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika sedang di Production (Railway)
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
